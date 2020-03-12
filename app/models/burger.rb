a0b1b25a087a043be19fec7eef8d70d341e02992
Hassan Bouzid <hbouzid@utbm.fr>
2020-03-12 09:22:07 +0100

    Liaison api

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,13 @@
 class Burger < ApplicationRecord
+    def nutriments 
+        #Recupérer donnée api --> Faire tableau 
+        # codeBurger = "3029330003533"
+        codeBurger = self.code
+        product = Openfoodfacts::Product.get(codeBurger,locale:'fr')
+        if product
+            return product.nutriments.to_hash
+        else
+            return[""]
+        end
+    end
 end
